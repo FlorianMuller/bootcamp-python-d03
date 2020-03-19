1114f4bc7bcbd30b1e91ff8e6dd1f7a21353de26
Florian Muller <fmuller@student.42.fr>
2020-03-19 04:25:14 +0100

    ex01 done

---
+++ ex01/ImageProcessor.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+class ImageProcessor:
+    @staticmethod
+    def load(path):
+        img = mpimg.imread(path)
+        print(f"Loading image of dimensions {img.shape[0]} x {img.shape[1]}")
+        return img
+
+    @staticmethod
+    def display(array):
+        plt.imshow(array)
+        plt.show()
+
+
+if __name__ == "__main__":
+    imp = ImageProcessor()
+
+    arr = imp.load("./42AI.png")
+    # arr = imp.load("./daruma.jpg")
+    print(repr(arr))
+
+    imp.display(arr)
