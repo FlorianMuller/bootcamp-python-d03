382f03df2437fc904d9fc4b80977e011908eeff0
Florian Muller <fmuller@student.42.fr>
2020-06-01 19:33:56 +0200

    add to_graysclae

--- ex03/ColorFilter.py
+++ ex03/ColorFilter.py
@@ -39,6 +39,15 @@ class ColorFilter:
         cel_shading = np.vectorize(cel_shading)
         return cel_shading(array)
 
+    @staticmethod
+    def to_grayscale(array, filter="w"):
+        if filter in ["w", "‘weighted’"]:
+            new_array = (array * [0.299, 0.587, 0.114]).sum(axis=2, keepdims=True).astype(int)
+            return np.tile(new_array, (1, 1, 3))
+        elif filter in ["m", "mean"]:
+            new_array = (array.sum(axis=2, keepdims=True) / 3).astype(int)
+            return np.broadcast_to(new_array, (*new_array.shape[:2], 3))
+
 
 if __name__ == "__main__":
     ip = ImageProcessor
@@ -46,7 +55,6 @@ if __name__ == "__main__":
 
     img = ip.load("./ex03/Igor.jpg")
     # img = ip.load("./ex03/elon.jpg")
-    ip.display(img)
 
     # Invert
     ip.display(cf.invert(img))
@@ -64,3 +72,9 @@ if __name__ == "__main__":
     for i in range(2, 7):
         print(f"Treshold {i}")
         ip.display(cf.celluloid(img, treshold=i))
+
+    # Greyscale
+    print("w")
+    ip.display(cf.to_grayscale(img))
+    print("m")
+    ip.display(cf.to_grayscale(img, filter="m"))
